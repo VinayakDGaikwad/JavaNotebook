bf2f866ba9f6bd5db913808774e8ec3c3c5c970b
vinayak <vinayakdwarkobagaikwad@gmail.com>
2023-09-23 09:58:58 +0530

    .

---
+++ CountFrequency.java
@@ -0,0 +1,18 @@
+public class CountFrequency {
+    public static void main(String[] args) {
+        int array[] = {3,4,5,6,7,44,44,4,4,4,4,44,4};
+
+        System.out.print(getFrequency(array,4));
+
+    }
+
+    static int getFrequency(int array[], int key){
+        int fre = 0;
+        for (int c : array){
+            if (c == key){
+                fre++;
+            }
+        }
+        return fre;
+    }
+}
